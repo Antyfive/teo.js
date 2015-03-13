ff1b74fa087b9a1e740add1e2f9449fcda407415
Andrew Teologov <teologov.and@gmail.com>
2015-03-13 23:53:29 +0200

    adding logger

---
+++ core/teo.logger.js
@@ -0,0 +1,47 @@
+/*!
+ * Logger module
+ * @author Andrew Teologov <teologov.and@gmail.com>
+ * @date 3/12/15
+ */
+
+var moment = require("moment");
+var colors = require("colors");
+var util = require("util");
+
+var logger = module.exports = {
+    success: function(message) {
+        var message = parseMessage(message);
+        console.log(format(util.format("Success: %s", message)).green);
+    },
+    info: function(message) {
+        var message = parseMessage(message);
+        console.log(format(util.format("Info: %s", message)).blue);
+    },
+    warn: function(message) {
+        var message = parseMessage(message);
+        console.log(format(util.format("Warn: %s", message)).yellow);
+    },
+    error: function(message) {
+        var message = parseMessage(message);
+        console.log(format(util.format("Error: %s", message)).red);
+    },
+    fatal: function(message) {
+        var message = parseMessage(message);
+        console.log(format(util.format("Fatal Error: %s", message)).red);
+    }
+};
+
+function parseMessage(message) {
+
+    try {
+        var message = (typeof message === "string") ? message : JSON.stringify(message);
+    } catch(e) {
+        logger.error(e.message);
+    }
+
+    return message;
+}
+
+function format(message) {
+    return util.format("[%s] %s", moment(), message);
+}
---
+++ test/specs/LoggerSpec.js
@@ -0,0 +1,82 @@
+/*!
+ * Logger tests
+ * @author Andrew Teologov <teologov.and@gmail.com>
+ * @date 3/12/15
+ */
+/* global define, describe, beforeEach, afterEach, it, assert, sinon, teoBase  */
+
+var logger = require(teoBase + "/teo.logger");
+var util = require("util");
+
+describe("Testing Logger", function() {
+
+    var formatSpy,
+        consoleLogStub,
+        message = "my message";
+
+    beforeEach(function() {
+
+        formatSpy = sinon.spy(util, "format");
+        consoleLogStub = sinon.stub(console, "log", function(message) {
+            return message;
+        });
+
+    });
+
+    afterEach(function() {
+
+        formatSpy.restore();
+        consoleLogStub.restore();
+
+    });
+
+    it("Should log success message", function() {
+
+        logger.success(message);
+
+        assert.deepEqual(formatSpy.args[0], ["Success: %s", message], "Message should be formatted correctly");
+
+    });
+
+    it("Should log info message", function() {
+
+        logger.info(message);
+
+        assert.deepEqual(formatSpy.args[0], ["Info: %s", message], "Message should be formatted correctly");
+
+    });
+
+    it("Should log warn message", function() {
+
+        logger.warn(message);
+
+        assert.deepEqual(formatSpy.args[0], ["Warn: %s", message], "Message should be formatted correctly");
+
+    });
+
+    it("Should log error message", function() {
+
+        logger.error(message);
+
+        assert.deepEqual(formatSpy.args[0], ["Error: %s", message], "Message should be formatted correctly");
+
+    });
+
+    it("Should log fatal error message", function() {
+
+        logger.fatal(message);
+
+        assert.deepEqual(formatSpy.args[0], ["Fatal Error: %s", message], "Message should be formatted correctly");
+
+    });
+
+    it("Should format final message correctly", function() {
+
+        logger.success(message);
+
+        assert.equal(formatSpy.args[1][0], "[%s] %s", "Format of final message should be correct");
+        assert.equal(formatSpy.args[1][2], "Success: " + message, "Message should be formatted correctly");
+
+    });
+
+});
