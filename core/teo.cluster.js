0e402ff374de75ed8cc2f311406d951b7a25d5bc
Andrew Teologov <teologov.and@gmail.com>
2015-04-07 20:55:37 +0300

    adding cluster spec

--- core/teo.cluster.js
+++ core/teo.cluster.js
@@ -4,15 +4,15 @@
  * @date 4/5/15
  */
 
-/* global logger  */
+/* global logger */
 
 var cluster = require("cluster"),
-    os = require("os"),
-    cpuCount = os.cpus().length;
+    os = require("os");
 
 exports = module.exports = function Cluster(callback) {
     // Code to run if we're in the master process
     if (cluster.isMaster) {
+        var cpuCount = os.cpus().length;
         // Create a worker for each CPU
         for (var i = 0; i < cpuCount; i++) {
             cluster.fork();
