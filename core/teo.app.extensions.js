036ba1e69086e0e38e17d0d92cfba70a0077755a
Andrew Teologov <teologov.and@gmail.com>
2015-05-10 14:11:31 +0300

    continue work on extensions

--- core/teo.app.extensions.js
+++ core/teo.app.extensions.js
@@ -10,16 +10,77 @@
  * Add local extensions (files)
  */
 var Base = require("./teo.base"),
-    _ = require("./teo.utils");
+    _ = require("./teo.utils"),
+    Path = require("path");
 
 exports = module.exports = Base.extend({
     initialize: function(opts) {
         this._extensionsRegistry = {};
+
+        _.extend(this, {
+            filePath: opts.filePath
+        });
+
+        if (opts.extensions) {
+            this.add(opts.extensions);
+        }
     },
 
+    /**
+     * Add new extension
+     * @param {Array|Object} extensions
+     */
     add: function(extensions) {
         var extensions = _.isArray(extensions) ? extensions : [extensions];
 
+        extensions.forEach(function(extension) {
+            this._resolveExtension(extension);
+        }.bind(this));
+    },
 
+    /**
+     * Resolves extension
+     * @param {Object} extension
+     * @private
+     */
+    _resolveExtension: function(extension) {
+        if (!_.isObject(extension)) {
+            throw new Error("Extension config should be an object");
+        }
+
+        if (!extension.hasOwnProperty("file") && !extension.hasOwnProperty("module")) {
+            throw new Error("Extension config should have 'module' or 'file' property");
+        }
+
+        if(!extension.hasOwnProperty("name")) {
+            throw new Error("Extension config should have 'name' property");
+        }
+        var pathToExt = extension.hasOwnProperty("file") ?
+                Path.join(this.filePath, extension.file) :  // local extension
+                    extension.module;   // module
+
+        var _extension = this.__requireExtension(pathToExt);
+
+        this._extensionsRegistry[extension.name] = _extension;
+
+    },
+
+    /**
+     * Requires extension
+     * @param {String} path
+     * @returns {*}
+     * @private
+     */
+    __requireExtension: function(path) {
+        var _extension;
+
+        try {
+            _extension = require(path);
+        } catch(e) {
+            logger.error(e);
+            throw new Error(e.message);
+        }
+
+        return _extension;
     }
 });
--- test/specs/AppExtensions.js
+++ test/specs/AppExtensions.js
@@ -9,11 +9,8 @@ var AppModules = require(teoBase + '/teo.app.extensions');
 
 describe("Testing Teo.js App Extensions", function () {
 
-    var modules;
-
-    beforeEach(function () {
-
-        var extension = function() {
+    var extensions,
+        extension = function() {
             return {
                 // is used in app config with this namespace
                 configNamespace: "my-module-config",
@@ -21,7 +18,7 @@ describe("Testing Teo.js App Extensions", function () {
                 config: {
                     "myParam": "1"
                 },
-                extension: function(app) {
+                install: function(app) {
                     app.middleware(function(req, res, next) {
                         req.setHeader("X-Powered-By", "Teo.js");
                         next();
@@ -30,7 +27,56 @@ describe("Testing Teo.js App Extensions", function () {
             }
         };
 
-        var extensionConfig = [
+    beforeEach(function () {
+
+        extensions = new AppModules({
+            filePath: "/extensions"
+            /*extensions: [
+                {
+                    "name": "my-extension-1",
+                    "module": "my-module-name-1"
+                },
+                {
+                    "name": "my-extension-2",
+                    "file": "my-module-name-2"
+                }
+            ]*/
+        });
+
+    });
+
+    afterEach(function () {
+
+        extensions = null;
+
+    });
+
+    it("Should parse passed config on init", function() {
+
+        assert.equal(extensions.filePath, "/extensions", "File path should be set");
+
+    });
+
+    it("Should resolve passed extensions on initialization", function() {
+
+        var addStub = sinon.stub(extensions, "add", function() {});
+
+        extensions.initialize({
+            extensions: [
+                {
+                    "name": "my-extension-1",
+                    "module": "my-module-name-1"
+                },
+                {
+                    "name": "my-extension-2",
+                    "file": "my-module-name-2"
+                }
+            ]
+        });
+
+
+        assert.isTrue(addStub.calledOnce, "Extensions should be added");
+        assert.deepEqual(addStub.args[0][0], [
             {
                 "name": "my-extension-1",
                 "module": "my-module-name-1"
@@ -39,19 +85,80 @@ describe("Testing Teo.js App Extensions", function () {
                 "name": "my-extension-2",
                 "file": "my-module-name-2"
             }
-        ];
+        ], "Arguments should be correct");
 
-        modules = new AppModules();
 
-    });
-
-    afterEach(function () {
-
-        modules = null;
+        addStub.restore();
 
     });
 
-    it("Should parse passed config on init", function() {
+    it("Should throw error on adding extension with not correct config", function() {
+
+        assert.throw(function() {
+            extensions.add();
+        }, "Extension config should be an object");
+
+        assert.throw(function() {
+            extensions.add({});
+        }, "Extension config should have 'module' or 'file' property");
+
+        assert.throw(function() {
+            extensions.add({file: "aaa"});
+        }, "Extension config should have 'name' property");
+
+    });
+
+    it("Should throw error on resolving of extension", function() {
+
+        var resolveSpy = sinon.spy(extensions, "_resolveExtension");
+
+        var add = function() {
+            extensions.add({
+                "name": "my-extension-1",
+                "module": "my-module-name-1"
+            });
+        };
+
+        assert.throw(add, "Cannot find module 'my-module-name-1'");
+        assert.isTrue(resolveSpy.calledOnce, "Resolve method should be called");
+
+        resolveSpy.restore();
+
+    });
+
+    it("Should resolve extension and add it to registry", function() {
+
+        var requireStub = sinon.stub(extensions, "__requireExtension");
+
+        requireStub.withArgs("my-module-name-1").returns({
+            // is used in app config with this namespace
+            configNamespace: "my-module-config",
+            // module's config
+            config: {
+                "myParam": "1"
+            }
+        });
+
+        var resolveSpy = sinon.spy(extensions, "_resolveExtension");
+
+        extensions.add({
+            "name": "my-extension-1",
+            "module": "my-module-name-1"
+        });
+
+        assert.isTrue(resolveSpy.calledOnce, "Resolve method should be called");
+        assert.isTrue(requireStub.calledOnce, "Extension should be loaded");
+        assert.equal(requireStub.args[0][0], "my-module-name-1", "Extension should be loaded");
+        assert.deepEqual({
+            // is used in app config with this namespace
+            configNamespace: "my-module-config",
+            // module's config
+            config: {
+                "myParam": "1"
+            }
+        }, extensions._extensionsRegistry["my-extension-1"], "Extensions registry should be updated");
+
+        resolveSpy.restore();
 
     });
 
