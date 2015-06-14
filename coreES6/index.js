ed8736342c5ae25220987f1ec3da900d05d4bd0c
Andrew Teologov <teologov.and@gmail.com>
2015-06-14 19:14:53 +0300

    minor improvements of client dispatcher, and utils

---
+++ coreES6/index.js
@@ -0,0 +1,10 @@
+/*!
+ * Teo.js entry point
+ * @author Andrew Teologov <teologov.and@gmail.com>
+ * @date 6/14/15
+ */
+
+const Teo = require("./teo");
+
+module.exports = Teo;
+module.exports.Teo = Teo;
