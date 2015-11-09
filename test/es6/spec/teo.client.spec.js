3bd447861e494a555e04175995de1f7cfd2a6b36
Andrew Teologov <teologov.and@gmail.com>
2015-11-09 22:45:43 +0200

    Initial set of client unit tests

--- test/es6/spec/teo.client.spec.js
+++ test/es6/spec/teo.client.spec.js
@@ -3,3 +3,120 @@
  * @author Andrew Teologov <teologov.and@gmail.com>
  * @date 11/8/15
  */
+
+/* global define, describe, beforeEach, afterEach, it, assert, sinon, teoBase  */
+
+"use strict";
+
+const
+    http = require("http"),
+    Client = require(`${teoBase}/teo.client`),
+    ClientContext = require(`${teoBase}/teo.client.context`);
+
+describe.only("Testing Teo Client", () => {
+
+    let client,
+        appDir = process.cwd().replace( /\\/g, "/"),
+        params = {
+            homeDir : appDir,
+            appDir  : appDir + "/apps/test",
+            confDir : appDir + "/config",
+            mode    : "test",
+            dilimiters : "{{ }}"
+        },
+        paramsStub = {
+            "config": {
+                "get": sinon.stub()
+            }
+        },
+        server, req, res;
+
+    before((done) => {
+
+        server = http.createServer((_req, _res) => {
+            paramsStub.req = _req;
+            paramsStub.res = _res;
+            _res.end();
+        }).listen(3210);
+
+        paramsStub.config.get.withArgs("appDir").returns(params.appDir);
+        paramsStub.config.get.withArgs("delimiters").returns(params.delimiters);
+
+        server.once("listening", done);
+
+    });
+
+    beforeEach((done) => {
+
+        http.get("http://localhost:3210", () => {
+            client = new Client(paramsStub);
+            done();
+        });
+
+    });
+
+    afterEach(() => {
+
+        client = null;
+
+    });
+
+    after((done) => {
+
+        req = res = params = appDir = paramsStub = null;
+        server.close(done);
+
+    });
+
+    describe("Initialize", () => {
+
+        it("Should create new client instance", () => {
+
+            let _client = Client.Factory(paramsStub);
+            assert.instanceOf(_client, Client);
+
+        });
+
+        it("Should create context", () => {
+
+            assert.instanceOf(client.context, ClientContext);
+
+        });
+
+        it("Should handle request error event fired by client.req", () => {
+
+            let resSendStub = sinon.stub(client.res, "send", function() {});
+
+            client.req.emit("error", 123, "Message");
+
+            assert.isTrue(resSendStub.calledOnce);
+            assert.deepEqual(resSendStub.args[0], [123, "Message"]);
+
+            resSendStub.restore();
+
+        });
+
+        it("Shouldn't match route not existing route", () => {
+
+            assert.isTrue(client.hasOwnProperty("route"));
+            assert.isUndefined(client.route);
+
+        });
+
+        it("Should have extension property", () => {
+
+            assert.isTrue(client.hasOwnProperty("extension"));
+            assert.equal(client.extension, "");
+
+        });
+
+    });
+
+    describe("Getters", () => {
+
+
+
+    });
+
+
+});
