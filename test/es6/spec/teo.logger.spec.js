6941d7595e75c84c193c937a15699c764b17cc3a
Andrew Teologov <teologov.and@gmail.com>
2015-11-15 11:40:15 +0200

    fixed tests

--- test/es6/spec/teo.logger.spec.js
+++ test/es6/spec/teo.logger.spec.js
@@ -88,7 +88,7 @@ describe("Testing Teo Logger", () => {
         logger.success(message);
 
         assert.equal(formatSpy.args[1][0], "[%s] %s", "Format of final message should be correct");
-        assert.equal(formatSpy.args[1][2], `Success: ${message}`, "Message should be formatted correctly");
+        assert.equal(formatSpy.args[1][2], `\u001b[32mSuccess: ${message}\u001b[39m`, "Message should be formatted correctly");
 
     });
 
@@ -96,7 +96,7 @@ describe("Testing Teo Logger", () => {
 
         logger.success("One", "two");
 
-        assert.equal(formatSpy.args[1][2], "Success: One|two", "Message should be formatted correctly");
+        assert.equal(formatSpy.args[1][2], "\u001b[32mSuccess: One|two\u001b[39m", "Message should be formatted correctly");
 
     });
 
