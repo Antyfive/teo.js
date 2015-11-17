d41323c6ec62b5a92fb21a0d0b1bddc9e04ed9a0
Andrew Teologov <teologov.and@gmail.com>
2015-11-17 20:35:30 +0200

    added first gulp task (test)

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,31 @@ const gulp = require("gulp"),
     runSequence = require("run-sequence"),
     config = require("config");
 
-process.on('uncaughtException', function(err) {
+process.on("uncaughtException", (err) => {
     console.error(err.message, err.stack, err.errors);
     process.exit(255);
 });
+
+function requireTask(path) {
+    let args = [].slice.call(arguments, 1);
+    return function(callback) {
+        let task = require(path).apply(this, args);
+
+        return task(callback);
+    };
+}
+
+gulp.on("err", (gulpErr) => {
+    if (gulpErr.err) {
+        // cause
+        console.error("Gulp error details", [gulpErr.err.message, gulpErr.err.stack, gulpErr.err.errors].filter(Boolean));
+    }
+});
+
+// tasks ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+gulp.task("test", requireTask('./tasks/test', {
+    src: "test/es6/**/*.js",
+    reporter: "spec",
+    require: ["babel/register", "./test/es6/common.js"]
+}));
