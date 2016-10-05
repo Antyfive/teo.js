fb56136095db8399eda5335de0ea3b65a2a20e2e
Andrew Teologov <teologov.and@gmail.com>
2016-10-05 21:49:52 +0300

    improved test cov

--- test/specs/core/teo.cluster.spec.js
+++ test/specs/core/teo.cluster.spec.js
@@ -12,15 +12,12 @@ const
     cluster = require("cluster"),
     os = require("os");
 
-describe("Testing Teo Cluster", () => {
+describe("Testing Teo.JS Cluster", () => {
 
     beforeEach(() => {
 
-        sinon.stub(os, "cpus", function() {
-            return new Array(2);
-        });
-
-        sinon.stub(cluster, "fork", function() {});
+        sinon.stub(os, "cpus", () => new Array(2));
+        sinon.stub(cluster, "fork", () => {});
 
     });
 
@@ -28,6 +25,7 @@ describe("Testing Teo Cluster", () => {
 
         os.cpus.restore();
         cluster.fork.restore();
+        cluster.removeAllListeners('exit');
 
     });
 
@@ -43,17 +41,42 @@ describe("Testing Teo Cluster", () => {
 
             assert.isTrue(spy.calledOnce, "Callback should be called");
             cluster.isMaster = true;
+
             done();
 
         })
 
     });
 
-    it("Should fork two workers", () => {
+    it("Should fork two workers", (done) => {
 
         new Cluster(function() {});
 
-        assert.isTrue(cluster.fork.calledTwice, "Fork two workers");
+        process.nextTick(() => {
+
+            assert.isTrue(cluster.fork.calledTwice, "Fork two workers");
+
+            done();
+
+        });
+
+    });
+
+    it("Should fork new cluster on `exit` event", (done) => {
+
+        new Cluster(function() {});
+
+        cluster.fork.reset();
+
+        cluster.emit('exit', {id: 'test'});
+
+        process.nextTick(() => {
+
+            assert.isTrue(cluster.fork.calledOnce, "Fork two workers");
+
+            done();
+
+        });
 
     });
 
